aed2e5959c5f3761a5d32f14d322453cd87c2d06
Jirka Korejtko <gorge@ufoni.cz>
2013-05-05 01:32:05 +0200

    Added discount banner Fixed image dimensions

--- index.php
+++ index.php
@@ -8,13 +8,17 @@
         <script src="modules/smooth_gallery/jd.gallery.js" type="text/javascript"></script>';
 	include("includes/hlavicka.php");
 ?>
+<div class="well well-blue text-center">
+  <h1><a href="typove-domy.php">Sleva ve výši 5 % na všechny uvedené ceny!</a></h1>
+  <p>Platí při <a href="kontakt.php">objednání</a> nezávazného osobního jednání do 15.&nbsp;6.&nbsp;2013.</p>
+</div>
 <?php include("includes/slideshow.php"); ?>
             <div class="row-fluid">
                 <div class="span8 well">
                     <h3>Levné stavby na celý život!</h3>
 
                     <p>Firma se od svého vzniku v&nbsp;roce 2005 zabývá realizací a projekcí nízkoenergetických zděných staveb na klíč s&nbsp;řadou referencí hotových rodinných domů a projektů.</p>
-                    <img src="images/fotka_home.png" width="250" height="140" alt="fotka home" style="float: left; margin-right: 10px;" />
+                    <img src="images/fotka_home.png" alt="fotka home" style="float: left; margin-right: 10px; width: 250px;" />
                     <p>Již při navrhování staveb dbáme na nízkou následnou nákladovost bydlení a pořizovací cenu stavby, která se ve většině případů pohybuje níže než u dřevostaveb, při zachování veškerých výhod nízko energetických zděných staveb.</p>
                     <p>Neváhejte nás kontaktovat a my stavbu navrhneme, popřípadě si můžete vybrat z&nbsp;nabídky některých naších typových projektů (lze je jakkoliv upravovat) a také můžeme zhotovit nezávaznou cenovou nabídku na Váš již zakoupený projekt.</p>
                     <p>V&nbsp;případě následné realizaci minimálně hrubé stavby, připravíme projektovou dokumentaci a vyřídíme stavební povolení (ohlášení stavby) zcela zdarma.</p>
