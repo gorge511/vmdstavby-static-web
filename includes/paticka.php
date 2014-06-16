ff0ebb729abae6e196e93cbc19676ab5a621b29d
Jirka Korejtko <gorge@ufoni.cz>
2014-06-16 18:35:00 +0200

    Added Sklik retargeting

--- includes/paticka.php
+++ includes/paticka.php
@@ -3,6 +3,12 @@
                     <p class="muted credit">Created and designed by Jan Baťha and company.</p><?php include("toplist.php"); ?>
                 </div>
             </div>
-        </div>  					
+        </div>
+        <script type="text/javascript">
+		/* <![CDATA[ */
+		var seznam_retargeting_id = 16422;
+		/* ]]> */
+		</script>
+		<script type="text/javascript"  src="//c.imedia.cz/js/retargeting.js"></script>
     </body>
 </html>
