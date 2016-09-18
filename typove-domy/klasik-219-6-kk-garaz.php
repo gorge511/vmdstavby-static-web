5861cecd6c3bacb2288c880a2c02f97cf0d887eb
Jirka Korejtko <korejtko@avast.com>
2016-09-18 23:16:50 +0200

    + Added 14 new houses

---
+++ typove-domy/klasik-219-6-kk-garaz.php
@@ -0,0 +1,166 @@
+<?php
+    $title = "KLASIK 219 6+kk + garáž";
+	$active = "typove-domy";
+	include("../includes/hlavicka.php");
+?>
+            <div class="row-fluid">
+                <div class="span8 well">
+	<h2><?php echo $title; ?></h2>
+
+<div class="typovy-dum">
+<img style="float: left; margin-right: 20px;" alt="KLASIK 219" src="../images/typove_domy/KLASIK_219.jpg">
+<p><strong>Počet podlaží:</strong> 2</p>
+<p><strong>Dispozice:</strong> 6+kk + garáž</p>
+<p><strong>Zastavěná plocha:</strong> 218,5 m2</p>
+<p><strong>Stavba na klíč:</strong> od 4.700.000,- bez DPH</p>
+<p><strong>Hrubá stavba:</strong> od 2.400.000,- bez DPH</p>
+<p>Ceny uvedené pro základní vybavenost</p>
+<p>&nbsp;</p>
+
+<p>Dům je střední rozměrové kategorie a je vhodný pro čtyř až pěti člennou rodinu. Tvary oken atd. se můžou libovolně měnit. Projekt možno vzrcadlit popřípadě jakkoliv upravit.</p>
+<p>&nbsp;</p>
+
+<p><img alt="KLASIK 219" src="../images/typove_domy_pudorysy/KLASIK_219.jpg"></p>
+<p>&nbsp;</p>
+
+<p><strong>Základní vybavenost:</strong></p>
+<p>&nbsp;</p>
+<p class="center"><strong>Soupis dodaných prací včetně materiálů obsažených v ceně díla v základní vybavenosti</strong></p>
+<p class="center"><strong>Varianta YTONG – HELUZ + dodatečné zateplení</strong></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+<p><strong>Příprava staveniště:</strong></p>
+<p>- příprava staveniště nutná pro realizaci</p>
+<p>- zemní práce nutné před realizací stavby</p>
+<p>- vytyčení stavby dle PD</p>
+<p>- usazení na pozemek dle PD</p>
+<p>&nbsp;</p>
+
+<p><strong>Základy a základová deska:</strong></p>
+<p>- vyhloubení a vylití základových pásů včetně realizace potřebných průchodek dle PD</p>
+<p>- vyzdění a vybetonování ztraceného bednění nad úrovní základových pásů - výšky dle PD</p>
+<p>- vlastní betony základové desky včetně ležatých rozvodů kanalizace a vyústění průchodek </p>
+<p>- Hydro izolace lepenkou – max. STŘEDNÍ RADONOVÉ RIZIKO</p>
+<p>&nbsp;</p>
+
+<p><strong>Svislé konstrukce:</strong></p>
+<p>- Vyzdění veškerého nosného zdiva</p>
+
+<p><em>Varianta HELUZ:</em></p>
+<p>HELUZ PLUS 30 tl.300mm NA CELOPLOŠNÉ LEPILO včetně systémových . překladů + dodatečné zateplení viz úprava vnějších zdí </p>
+<p>Vyzdění vnitřních příček: HELUZ 11,5 na lepidlo včetně nenosných překladů</p>
+<p>&nbsp;</p>
+
+<p><em>Varianta YTONG:</em></p>
+<p>YTONG P2-400 tl.300mm NA CELOPLOŠNÉ LEPILO včetně systémových . překladů + dodatečné zateplení viz úprava vnějších zdí </p>
+<p>Vyzdění vnitřních příček: YTONG tl.100 na lepidlo včetně nenosných překladů</p>
+<p>&nbsp;</p>
+<p>- Vyhotovení železobetonového obvodového věnce nad nosným zdivem ve 2NP, sloužící taktéž pro ukotvení střešní KCE</p>
+<p>&nbsp;</p>
+
+<p><strong>Vodorovné konstrukce:</strong></p>
+<p><u>Strop 1.NP:</u> předpjaté dutinové panely dle PD včetně žel.bet. obvodového věnce</p>
+<p><u>Schodiště:</u> železobetonové schodiště zaklopené linoleem viz. finální podlahy včetně základního olištování</p>
+<p>&nbsp;</p>
+
+<p><strong>Zastřešení stavby:</strong></p>
+<p>Krovy: krokevní soustava dle PD, krovy v přesahu za vnější obvod hoblovány a shora pobité palubkami ošetřenými bezbarvým lakem</p>
+<p>Tašky: KM-BETA klasik včetně všech potřebných doplňků – ČERVENÁ, ČERNÁ A HNĚDÁ BARVA</p>
+<p>Okapy, svody a oplechování: Pobarvený plech okapní svody budou zakončeny nad úrovní rostlého terénu</p>
+<p>Hromosvod: dle elektro projektu</p>
+<p>&nbsp;</p>
+
+<p><strong>Zdravotechnika:</strong></p>
+<p><u>Voda:</u> rozvody podlahách a zdech přivedené na místo připojení zařizovacích předmětů dle PD a zaslepeny. Hlavní přívod vody k vodoměrné šachtě do 10m.</p>
+<p>Výroba teplé užitkové vody zabezpečí bojler 160 l.</p>
+<p><u>Kanalizace:</u> rozvody podlahách a zdech přivedené na místo připojení zařizovacích předmětů a zaslepeny. Hlavní přívod kanalizace k revizní kanalizační šachtě do 10m.</p>
+<p>&nbsp;</p>
+
+<p><u>Zařizovací předměty zdravotechniky:</u></p>
+<p>2 x standardní WC kombi v ceně včetně ovládacího zařízení do 2.500,- /kus včetně DPH</p>
+<p>2 x umyvadlo včetně baterie v ceně do 2.500,- /kus včetně DPH</p>
+<p>1 x sprchový kout stavebnice s vlastní baterií v celkové ceně do 8.000,- /kus včetně DPH</p>
+<p>1 x vana včetně baterie v ceně do 6.500,- /kus včetně DPH</p>
+<p>&nbsp;</p>
+
+<p><strong>Ústřední topení:</strong></p>
+<p>Zdrojem tepelné energie bude elektrokotel dle PD. Vlastní vytápění bude zabezpečeno celoplošným podlahovým vytápěním bez systémových desek – kotvení do podlahového polystyrénu na skoby. Mechanický pákový rozdělovač okruhů v systémové skříni.</p>
+<p>&nbsp;</p>
+
+<p><strong>Elektroinstalace:</strong></p>
+<p>Veškeré rozvody kabeláže ve zdech a podlahách pro zásuvky, osvětlení, varnou desku, taktéž pro 1 x vnější zásuvku na 220V a 380V. V místě zásuvek a vypínačů krabice s kabeláží připravené pro jejich instalaci. V místě světel kabeláž ze stropu připravená pro napojení daného osvětlení. V místě připojení varné desky krabice s kabeláží pro připojení. V místě Bojleru krabice s kabeláží pro připojení. Vnitřní domovní rozvaděč s potřebnými jističi dle PD. Přivedení silového kabelu k vnějšímu rozvaděči RM do 10m. Osazení vypínačů a zásuvek modelu KLASIK výrobce ABB standard bílé barvy. Vlastní osvětlovací zařízení (lustry, svítidla, bodovky atd.) nejsou součástí dodávky. Počty zásuvek a světel jsou počítány dle PD v nižším standardu.</p>
+<p>Součástí dodávky nejsou nízkonapěťové, televizní a datové rozvody. </p>
+<p>- kanalizační rozvody, rozvody TUV + SUV a elektro související s kuchyňskou linkou budou rozvedeny do předpokládaného místa připojení a zaslepeny.</p>
+<p>&nbsp;</p>
+
+<p><strong>Výplně stavebních otvorů:</strong></p>
+
+<p><strong><em>Vnějších:</em></strong></p>
+<p><u>Okna, balkónové dveře:</u></p>
+<p>Dodavatel: <strong>VEKRA</strong></p>
+<p>Provedení: plastové 5-ti komorová profil <strong>PRIMA</strong></p>
+<p>Barevnost: vnější a vnitřní barva bílá stejná barevnost vnějších plechových a vnitřních plastových parapetů</p>
+<p>Zasklení: trojsklo bez okrasných vnitřních příček</p>
+<p><u>Vchodové dveře:</u></p>
+<p>Stejného systému jako okna s kováním vchodových dveří v patřičném profilu, výplň Izolační trojsklo popřípadě hladká deska</p>
+<p><u>Střešní okna:</u> jsou-li naprojektovány ROTO, VELUX základní </p>
+<p><u>Garážová vrata:</u> pokud je součástí stavby garáž – rolovací s el. pohonem na dálkové ovládání bílé do 26.000,- bez DPH/kus/stavební otvor</p>
+<p>&nbsp;</p>
+
+<p><strong><em>Vnitřních:</em></strong></p>
+<p><u>Vnitřní dveře s obložkami:</u></p>
+<p>SAPELI ELEGANT Model 10</p>
+<p>Vybavení: Standardní bezpříplatkové</p>
+<p>Sklo: dle výběru k danému tipu bezpříplatkové</p>
+<p>Povrch dveří i obložek: fólie - bílá, dub, buk, třešeň, javor, hruška, bělený dub, olše, teak, wenge</p>
+<p>Obložky: folie základního bezpříplatkového provedení potřebného typu a materiálu ve vybrané barevnosti</p>
+<p>Zámkový set a kliku: Z důvodu velkého výběru dodá investor sám, popřípadě bude řešena dodávka jako vícepráce</p>
+<p>&nbsp;</p>
+
+<p><strong>Finální úpravy povrchů zdí, stropů a schodiště:</strong></p>
+<p><u>Vnější zdivo:</u></p>
+<p>- Pohledová boční strana základové desky obložená tepelnou izolací XPS tl.40mm ve viditelných částech natažená lepidlo + perlinka a okrasná zrnitá kamenitá fasáda </p>
+<p>- Vnější nosné zdivo: fasádní polystyrén EPS-F 70 tl.150mm + perlina + lepidlo + fasádní akrilátová omítka bílé barvy zrnitosti 1mm (točená)  </p>
+<p><u>Vnitřní zdivo:</u></p>
+<p>VC malta + štuk popřípadě perlina+lepidlo+štuk</p>
+<p><u>Podhled stropů 1.NP:</u></p>
+<p>Sádrokartón začištěný + s tepelnou izolací vatou URSA tl.260mm</p>
+<p><u>Betonové mazaniny finálních podlah:</u></p>
+<p>- Podkladová tepelná izolace EPS-S dle PD a vlastní betony strojem hlazené</p>
+<p><u>Vnitřní vymalování:</u></p>
+<p>- Bílou barvou PRIMALEX</p>
+<p>&nbsp;</p>
+
+<p><strong>Finální podlahy:</strong></p>
+<p><em>Koupelny (popřípadě koupelny s WC):</em> keramická dlažba a obklad do výšky stropů v ceně do 300,-/m2 včetně DPH bez liš a listel</p>
+<p><em>Samostatné WC, zádveří a technické místnosti:</em> keramická dlažba se soklem 8cm v ceně do 300,-/m2 včetně DPH bez liš a listel</p>
+<p>Ostatní: linoleum popř. koberce v katalogové ceně do 300,-/m2 včetně DPH</p>
+<p>&nbsp;</p>
+
+<p><strong>Venkovní terasy včetně jejich základů:</strong> Není součástí dodávky.</p>
+<p>&nbsp;</p>
+
+<p><strong>Garážové stání:</strong> Není součástí dodávky</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+<p class="center"><strong>Některé z možných víceprací:</strong></p>
+<p>&nbsp;</p>
+
+<p>1.) Komín HELUZ popř. SIKO s okrasnou imitací cihel nad úrovní střechy dle výběru dodavatel (bílá, červená), přívod vzduchu ve finálních podlahách k topnému mediu.</p>
+<p>&nbsp;</p>
+<p><strong>cena: 75.000,- bez DPH</strong></p>
+<p>&nbsp;</p>
+
+<p>2.) Vnější fasáda v světlé barvě (bezpříplatkový odstín)</p>
+<p>&nbsp;</p>
+<p><strong>cena: 45.000,- bez DPH</strong></p>
+<p>&nbsp;</p>
+
+<p>Další možné vícepráce rádi doceníme dle vašich požadavků.</p>
+</div>
+                </div>
+<?php include("../includes/side-kontakt.php"); ?>
+            </div>
+<?php include("../includes/paticka.php"); ?>
