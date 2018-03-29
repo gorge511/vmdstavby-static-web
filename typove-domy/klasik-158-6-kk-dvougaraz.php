5521c2089d29b8fc49118b9ccfff1ecc36dc4b61
Jirka Korejtko <korejtko@avast.com>
2018-03-29 21:18:34 +0200

    + Add new houses

---
+++ typove-domy/klasik-158-6-kk-dvougaraz.php
@@ -0,0 +1,166 @@
+<?php
+    $title = "KLASIK 158 6+kk + dvougaráž";
+	$active = "typove-domy";
+	include("../includes/hlavicka.php");
+?>
+            <div class="row-fluid">
+                <div class="span8 well">
+	<h2><?php echo $title; ?></h2>
+
+<div class="typovy-dum">
+<img style="float: left; margin-right: 20px;" alt="KLASIK 158" src="../images/typove_domy/KLASIK_158.jpg">
+<img style="float: left; margin-right: 20px;" alt="KLASIK 158" src="../images/typove_domy/KLASIK_158b.jpg">
+<img style="float: left; margin-right: 20px;" alt="KLASIK 158" src="../images/typove_domy/KLASIK_158c.jpg">
+<img style="float: left; margin-right: 20px;" alt="KLASIK 158" src="../images/typove_domy/KLASIK_158d.jpg">
+<p><strong>Počet podlaží:</strong> 2</p>
+<p><strong>Dispozice:</strong> 6+kk + dvougaráž</p>
+<p><strong>Zastavěná plocha:</strong> 158,25 m2</p>
+<p><strong>Stavba na klíč:</strong> od 4.900.000,- bez DPH</p>
+<p><strong>Hrubá stavba:</strong> od 2.200.000,- bez DPH</p>
+<p>Ceny uvedené pro základní vybavenost</p>
+<p>&nbsp;</p>
+
+<p>Dům je větší rozměrové kategorie a je vhodný pro pěti až sedmi člennou rodinu. Projekt možno vzrcadlit popřípadě jakkoliv upravit.</p>
+<p>&nbsp;</p>
+
+<p><img alt="KLASIK 158" src="../images/typove_domy_pudorysy/KLASIK_158.jpg"></p>
+<p>&nbsp;</p>
+
+<p><strong>Základní vybavenost:</strong></p>
+<p>&nbsp;</p>
+<p class="center"><strong>Soupis dodaných prací včetně materiálů obsažených v ceně díla v základní vybavenosti</strong></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+<p><strong>Příprava staveniště:</strong></p>
+<p>- realizační projekt stavby</p>
+<p>- příprava staveniště nutná pro realizaci</p>
+<p>- zemní práce nutné před realizací stavby</p>
+<p>- vytyčení stavby dle PD</p>
+<p>- usazení na pozemek dle PD</p>
+<p>&nbsp;</p>
+
+<p><strong>Základy a základová deska:</strong></p>
+<p>- vyhloubení a vylití základových pásů z prostého betonu včetně realizace potřebných průchodek dle PD. Pod nosným zdivem stavby a nosnými konstrukcemi únosnost zeminy uvažována min. 150MPa (určí průzkum IGP).</p>
+<p>- vyzdění a vybetonování ztraceného bednění nad úrovní základových pásů v.250mm včetně výplně vnitřního prostou mezi ztraceným bednění štěrkem a odvětrání desky proti radonu.</p>
+<p>- Hydro izolace 1 x lepenkou DEKGLASS G200S40 max. stření radonové riziko</p>
+<p>&nbsp;</p>
+
+<p><strong>Svislé konstrukce:</strong></p>
+
+<p><u>Vnější nosné zdivo 1.NP:</u> HELUZ FAMILY 50 – tl. 500mm (1. a 2.řada FAMILY 2in1 tl.440mm) NA CELOPLOŠNÉ LEPILO včetně překladů dle PD - s přípravou prostoru pro předokenní žaluzie v pozicích dle PD (vlastní předokenní žaluzie, včetně pohonů, vodících lišt a elektrické připravenosti nejsou součástí dodávky)</p>
+<p>- Vnější nosné zdivo 2.NP: HELUZ FAMILY 50 – tl. 500mm NA CELOPLOŠNÉ LEPILO včetně překladů dle PD - s přípravou prostoru pro předokenní žaluzie v pozicích dle PD (vlastní předokenní žaluzie, včetně pohonů, vodících lišt a elektrické připravenosti nejsou součástí dodávky)</p>
+<p>- Vnější atikové zdivo 2.NP a střechy 2.NP: <br>
+1. ŘADA ZDIVA v.250mm: HELUZ FAMILY 2in1 25 – tl. 250mm NA CELOPLOŠNÉ LEPILO<br>
+2. ŘADA ZDIVA v.250mm: žel. bet. věnec v.250mm š.250mm beton a výztuž dle PD<br></p>
+<p>- Vnitřní nosné zdivo: HELUZ UNI tl.250mm NA CELOPLOŠNÉ LEPILO </p>
+<p>- Vnitřní příčky: HELUZ 11,5 na lepidlo včetně nenosných překladů</p>
+<p>- Komín:<br>
+1x montáž jedno průduchového stavebnicového komína s doplňkovou vzduchovou šachtou výrobce (HELUZ, KOMSIKO, CIKO atd.) s okrasným cihlovým límcem nad úrovní střechy dle nabídky výrobce – verze odkouření pro krbová kamna průměr 200mm nebo 180mm dle požadavků dodavatele krbového systému. Krb nebo krbová kamna nejsou součástí dodávky</p>
+<p>&nbsp;</p>
+
+<p><strong>Vodorovné konstrukce:</strong></p>
+<p>- <u>Strop 1.NP a 2.NP:</u> železobetonová stropní deska v.250mm s výztuží a betony dle PD (s ohledem na konzolově vynesený balkon není výhodné použít jiný systém)</p>
+<p>- <u>Zábradlí balkonu 2.NP:</u> není součástí dodávky včetně přípravy kotvení atd.</p>
+<p>&nbsp;</p>
+
+<p><strong>Schodiště:</strong></p>
+<p><u>RD 1.NP – 2.NP žel. bet schodiště:</u></p>
+<p>- železobetonová konstrukce schodiště</p>
+<p>- finální povrch schodnic resp. zaklopením linoleem v kat. ceně do 250,- m2 bez DPH včetně standardních rohových základních lišt</p>
+<p>- bezpečnostní zábradlí otevřeného prostoru 2.NP (mimo výstupní stupeň) bude řešeno vyzděním z příčky tl.115mm (naházené VCM maltou + štuk + bílá barva) výšky 1000mm nad čistou podlahu 2.NP a horní hrana tohoto zábradlí bude obložená dřevěnou obložkou materiálu dub popř. buk lakovanou bezbarvým lakem. Otevřený prostor mezi stropem 1.NP a schodnicemi bude uzavřen ocelovými tyčemi s černým pohledovým nátěrem ve tvaru L kotvenými kratší stranou do boku schodnic a ke stropu 1.NP s osovou vzdáleností v délce schodnic.</p>
+<p>&nbsp;</p>
+
+<p><strong>Zastřešení stavby:</strong></p>
+<p>Atiková střecha nad garáží 1.NP a nad 2.NP: vyzděné atikové zdivo viz. svislé konstrukce včetně věnce, horní hrana stropů a vnitřní/horní strana zdiva vylepená asfaltovým pásem, zaklopení spádovým polystyrénem min. výšky 200mm a následné zaklopení PVC folii (přetaženou přes atikové zdivo) kotvenou bez nutného záklopu. Včetně všech systémových doplňků a střešními vpustmi dle PD a bezpečnostním přepadem.</p>
+<p>&nbsp;</p>
+
+<p><strong>Zdravotechnika:</strong></p>
+<p><u>Voda:</u> rozvody podlahách a zdech přivedené na místo připojení zařizovacích předmětů dle PD a zaslepeny. Hlavní přívod vody k vodoměrné šachtě. Výroba teplé vody zabezpečí bojler 160 l</p>
+<p><u>Kanalizace:</u> rozvody podlahách a zdech přivedené na místo připojení zařizovacích předmětů a zaslepeny. Hlavní přívod kanalizace k revizní kanalizační šachtě.</p>
+<p>&nbsp;</p>
+
+<p><u>Zařizovací předměty zdravotechniky:</u></p>
+<p>2 x závěsné WC obezděné do výšky modulu v ceně komplet včetně modulu, tlačítka, mísy, prkýnky atd. do 11.000,- /kus bez DPH z katalogových cen SIKO</p>
+<p>2 x umyvadlo včetně baterie v ceně do 7.000,- /kus bez DPH z katalogových cen SIKO</p>
+<p>2 x umyvadlo včetně baterie v ceně do 2.000,- /kus bez DPH z katalogových cen SIKO</p>
+<p>1 x sprchový kout včetně zástěny, kanálku, baterie atd. v celkové ceně do 20.000,- /kus bez DPH z katalogových cen SIKO</p>
+<p>1 x vana obezděná s vlastní baterií v celkové ceně do 12.000,- /kus bez DPH z katalogových cen SIKO</p>
+<p>&nbsp;</p>
+
+<p><strong>Ústřední topení:</strong></p>
+<p>Zdrojem tepelné energie bude elektrokotel dle PD. Vlastní vytápění teplovodní podlahové kotvené plastovými kotvi do podlahového polystyrénu EPS-S s 2 x mechanickým rozdělovačem okruhů připevněným na zdi.</p>
+<p>&nbsp;</p>
+
+<p><strong>Elektroinstalace:</strong></p>
+<p>Veškeré rozvody kabeláže ve zdech a podlahách pro zásuvky, osvětlení, varnou desku, taktéž pro 1 x vnější zásuvku na 220V a 380V. V místě zásuvek a vypínačů krabice s kabeláží připravené pro jejich instalaci. V místě světel kabeláž ze stropu připravená pro napojení daného osvětlení. V místě připojení varné desky krabice s kabeláží pro připojení. Zásuvka/krabice pro plynový kotel a systémy ÚT a vody a jejího ohřevu. Vnitřní domovní rozvaděč s potřebnými jističi dle PD. Přivedení silového kabelu k vnějšímu rozvaděči RM vystrojení a jištění vnějšího rozvaděče není uvažováno. Osazení vypínačů a zásuvek modelu TANGO standard bílé barvy. Vlastní osvětlovací zařízení (lustry, svítidla, bodovky atd.) nejsou součástí dodávky. Počty zásuvek a světel jsou počítány dle PD v nižším standardu.</p>
+<p>Součástí dodávky nejsou nízkonapěťové, televizní a datové rozvody.</p>
+<p>- kanalizační rozvody, rozvody TUV + SUV a elektro související s kuchyňskou linkou budou rozvedeny do předpokládaného místa připojení a zaslepeny.</p>
+<p>&nbsp;</p>
+
+<p><strong>Výplně stavebních otvorů:</strong></p>
+
+<p><strong><em>Vnějších:</em></strong></p>
+<p><u>Okna, balkónové dveře:</u></p>
+<p>Dodavatel: <strong>VEKRA</strong></p>
+<p>Provedení: plastové 5-ti komorová profil <strong>PRIMA</strong></p>
+<p>Barevnost: vnější a vnitřní barva bílá stejná barevnost vnějších plechových a vnitřních plastových parapetů</p>
+<p>Zasklení: trojsklo bez okrasných vnitřních příček</p>
+<p><u>1 x Hlavní vchodové dveře a 1 x pomocné vchodové dveře (garáž):</u></p>
+<p>Stejného systému a barevnosti jako okna s kováním vchodových dveří v patřičném profilu, výplň Izolační trojsklo popřípadě hladká deska</p>
+<p><u>Garážová vrata:</u> 2 x sekční s pohonem na dálkové ovládání v katalogové ceně včetně montáže 25.000,- bez DPH </p>
+<p><u>Předokenní žaluzie:</u> včetně pohonů, vodících lišt a elektrické připravenosti nejsou součástí dodávky</p>
+<p><u>Střešní okna:</u> nejsou součástí dodávky</p>
+<p><u>Zábradlí balkonu 2.NP:</u> není součástí dodávky</p>
+<p>&nbsp;</p>
+
+<p><strong><em>Vnitřních:</em></strong></p>
+<p><u>Vnitřní dveře s obložkami (2 x zašupovací dveře po zdi bez obložek se základní vodící konzolou):</u></p>
+<p>SAPELI ELEGANT Model 10</p>
+<p>Povrch dveří i obložek: fólie – barevnosti z bezpříplatkového výběru</p>
+<p>Zámkový set a kliku: SAPELI PRAKTIK 01 klika/klika v ceně do 250,- bez DPH/dveře</p>
+<p>&nbsp;</p>
+
+<p><strong>Finální úpravy povrchů zdí, stropů a schodiště:</strong></p>
+<p><u>Vnější zdivo:</u></p>
+<p>- Pohledová boční strana základové desky/soklová část pro 2.řadu zdiva 1.NP XPS/PERIMETR tl.20mm a ve viditelných částech natažená perlinka/lepidlo a okrasná zrnitá kamenitá fasáda marmolit základní zrnitosti a barevnosti </p>
+<p>- Zdivo VCM (vápno cementová termo izolační malta systému MAXIT) + penetrace + silikonová popř. silikátová finální fasáda bílé barvy točená zrnitosti 1,5mm   </p>
+<p><u>Vnitřní zdivo:</u></p>
+<p>VC malta + štuk</p>
+<p><u>Podhled stropů:</u></p>
+<p>1.NP Sádrokartón začištěný na rastru<br>
+2.NP Sádrokartón začištěný na rastru</p>
+<p><u>Betonové mazaniny finálních podlah:</u></p>
+<p>- Podkladová tepelná izolace tl.120mm v 1.NP a tl.30mm v 2.NP EPS-S dle PD a vlastní betony strojem hlazené</p>
+<p><u>Vnitřní vymalování:</u></p>
+<p>- Bílou barvou</p>
+<p>&nbsp;</p>
+
+<p><strong>Finální podlahy:</strong></p>
+<p><em>Koupelny a samostatná WC:</em> keramická dlažba a obklad do výšky stropů v ceně do 250,-/m2 bez DPH bez liš a listel z katalogových cen SIKO</p>
+<p><em>Technická místnost a garáž:</em> keramická dlažba a sok do výšky 80mm v ceně do 250,-/m2 bez DPH bez liš a listel z katalogových cen SIKO</p>
+<p><em>Balkon 2.NP: mrazuvzdorná keramická dlažba a sok do výšky 80mm v ceně do 250,-/m2 bez DPH bez liš a listel z katalogových cen SIKO</p>
+<p><em>Ostatní vnitřní místnosti:</em> PVC linoleum popř. koberce v katalogové ceně do 250,-/m2 včetně DPH</p>
+<p>&nbsp;</p>
+
+<p><strong>Součástí dodávky nejsou:</strong></p>
+<p>- vlastní předokenní žaluzie, včetně pohonů, vodících lišt a elektrické připravenosti nejsou součástí dodávky</p>
+<p>- zábradlí balkonu 2.NP včetně přípravy kotvení atd.</p>
+<p>-realizace vlastních přípojek voda, kanalizace a elektro a vše s nimi související</p>
+<p>- realizace vnějších teras a vším s nimi souvisejícím</p>
+<p>-finální terénní úpravy okolo stavby a na celém pozemku včetně realizace vnějších zpevněných ploch, plotů, likvidace dešťových vod atd.</p>
+
+<p class="center"><strong>Některé z možných víceprací:</strong></p>
+<p>&nbsp;</p>
+
+<p>1.) Zvýšení desky o každých dalších 250mm:</p>
+<p style="margin-left: 20px;">-	Ztracené bednění pod nosným zdivem a vynášenými prvky vyplněné betoném + výztuž a vyplnění prostoru mezi bedněním</p>
+<p><strong>Cena vícepráce: 40.000,- bez DPH /250mm výšky</strong></p>
+<p>&nbsp;</p>
+
+<p>Další možné vícepráce rádi doceníme dle vašich požadavků.</p>
+</div>
+                </div>
+<?php include("../includes/side-kontakt.php"); ?>
+            </div>
+<?php include("../includes/paticka.php"); ?>
